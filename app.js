9d1373550b8142cd738d9035cbd4551aca6bd002
Jami589 <154450574+Jami589@users.noreply.github.com>
2024-03-02 04:18:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+
+// Q1
+var one = 2
+var two = 4
+
+
+console.log (one + two)
+
+//Q2
+  
+console.log (two - one)
+ 
+//Q3
+
+console.log (one * two )
+
+//Q4
+
+console.log (two / one)
+
+ var one1 = 3
+ var two2  = 7
+ 
+ console.log (two2 % one )
+
+//Q5
+
+var exponentation = one **2
+console.log (exponentation)
+
+
+//Q6
+
+
+var math = 100
+var maths = 80
+
+
+var urdu = 100
+var urdus = 78
+
+ var english = 100
+ var englishs =84
+  
+ var chemistry =75
+ var chemistrys = 45
+
+ var physics = 75
+ var physicss = 45
+
+ var total = math + urdu + english + chemistry + physics
+ var obtain= maths+ urdus + englishs + chemistrys + physicss
+ 
+ console.log(total)
+ console.log (obtain)
+
+ var final = obtain *100 / total
+
+ console.log ( final)
+
+
+ //completse//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
